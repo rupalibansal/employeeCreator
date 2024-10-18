640225ada45bb1a8789ba8b112acaf64ff33cb2c
rupalibansal <r.rupali211@gmail.com>
2024-10-18 17:29:02 +1100

    added create and update to the service

--- frontEnd/src/services/employee-services.ts
+++ frontEnd/src/services/employee-services.ts
@@ -1,3 +1,4 @@
+import { EmployeeFormValues } from "./employeeSchema";
 const baseURL = import.meta.env.VITE_APP_API_BASE_URL;
 
 export interface Address {
@@ -26,7 +27,7 @@ export interface Employee {
   isPermanent: boolean;
 }
 
-export const getAllTodos = async () => {
+export const getAllEmployees = async () => {
   try {
     const response = await fetch(`${baseURL}/api/employees`, {
       method: "GET",
@@ -43,4 +44,86 @@ export const getAllTodos = async () => {
     console.error(error);
     throw new Error("Failed to fetch data");
   }
+};
+
+export const getEmployeeById = async (id: number) => {
+  const response = await fetch(`${baseURL}/api/employees/${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    if (response.status === 404) {
+      throw new Error(await response.text());
+    }
+    throw new Error("Something went wrong");
+  }
+  return (await response.json()) as Employee;
+};
+
+export const getEmployeeByDepartment = async (department: string) => {
+  const response = await fetch(
+    `${baseURL}/api/employees?department=${department}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    }
+  );
+  if (!response.ok) {
+    throw new Error("Failed to get Employees by Department");
+  }
+  return (await response.json()) as Employee[];
+};
+
+export const deleteEmployeeById = async (id: number) => {
+  const response = await fetch(`${baseURL}/api/employees/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to Delete the record");
+  }
+  return true;
+};
+
+export const createEmployee = async (data: EmployeeFormValues) => {
+  const response = await fetch(`${baseURL}/api/employees`, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  });
+  if (!response.ok) {
+    throw new Error("Failed to create Employee Record");
+  }
+  return (await response.json()) as Employee;
+};
+
+export const updateEmployeeById = async (
+  id: number,
+  data: EmployeeFormValues
+) => {
+  const response = await fetch(`${baseURL}/api/employees/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  if (!response.ok) {
+    throw new Error("Something went wrong");
+  }
+  return (await response.json()) as Employee;
 };
