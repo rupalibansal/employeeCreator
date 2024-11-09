bba5ad4701698832932db1cd9370211a0347cd4f
rupalibansal <r.rupali211@gmail.com>
2024-11-09 16:01:06 +1100

    fixed the test case

--- e2eTesting/employeeCreaterE2E/test.spec.ts
+++ e2eTesting/employeeCreaterE2E/test.spec.ts
@@ -2,9 +2,16 @@ import { test, expect, Browser, Page } from "@playwright/test";
 import { webkit, chromium, firefox } from "@playwright/test";
 
 test("verify the title of the page", async () => {
-  const browser: Browser = await firefox.launch({ headless: false });
+  const browser: Browser = await chromium.launch({
+    headless: false,
+    channel: "chrome",
+  });
   const newPage: Page = await browser.newPage();
   await newPage.goto("http://localhost:5173/");
   const title = await newPage.title();
-  await expect(newPage).toHaveTitle(title);
+  console.log("Title:", title);
+  await newPage.screenshot({ path: `./screenshots/title.png` });
+  await expect(title).toEqual("Employee Creator");
+  await newPage.waitForTimeout(10000);
+  browser.close();
 });
